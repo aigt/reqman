414102b4a1f5970367721f427647a90f63bc4a60
aigt <ai-kr@yandex.ru>
2017-09-17 13:33:23 +0500

    Bootstrap layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { StructureListComponent } from './structure-list/structure-list.component';
+import { StructureComponent } from './structure/structure.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/structure', pathMatch: 'full' },
-  { path: 'structure',  component: StructureListComponent },
-  { path: 'structure/:itemId',  component: StructureListComponent },
+  { path: 'structure',  component: StructureComponent },
+  { path: 'structure/:itemId',  component: StructureComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { StructureListComponent } from './structure-list/structure-list.component';
-import { StructureEntityComponent } from './structure-entity/structure-entity.component';
+import { StructureListComponent } from './structure/structure-list/structure-list.component';
+import { StructureEntityComponent } from './structure/structure-entity/structure-entity.component';
+import { StructureComponent } from './structure/structure.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StructureListComponent,
-    StructureEntityComponent
+    StructureEntityComponent,
+    StructureComponent
   ],
   imports: [
     BrowserModule,
